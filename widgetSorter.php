4a3c7bb10e29f7b025616f224ad2e236a04f76a6
Scott Andrews <scottandrews490@gmail.com>
2020-05-22 16:21:29 +0100

    got the main bulk of the function working - just adding in comments to explain my working

--- widgetSorter.php
+++ widgetSorter.php
@@ -4,20 +4,83 @@
 if($_POST['widgets']){
     $totalPackets = calculateWidgets($_POST['widgets']);
 
-    echo $totalPackets;
+    var_dump($totalPackets);
 }
-
-// Array of packets that are avaliable
-$packets = [250, 500, 1000, 2000, 5000];
 
 // Main function for calculating widgets
 function calculateWidgets($widgetsRequired){
+    // Array of packets that are avaliable
+    //$packets = [500, 250, 1000, 2000, 5000];    
+    $packets = array(500, 250, 1000, 2000, 5000);
+    $pickedPackets = [];
 
-    foreach($packets as $packet) {
-        echo $packet;
+    $remainder = $widgetsRequired;
+    
+    $pickedValue = 0;
+
+    sort($packets);
+
+    // Flips the array round to check from biggest to smallest
+    $flipPackets = array_reverse($packets);
+
+    while($remainder > 0){
+
+        // Filters out the odd usecase where instead of 500 x1 it says 250 x 2
+        if($remainder < $packets[1] && $remainder > 0){
+
+            if($remainder <= $packets[0]){
+
+                array_push($pickedPackets, $packets[0]);
+                $remainder = $remainder - $packets[0];
+
+            }else{
+
+                array_push($pickedPackets, $packets[1]);
+                $remainder = $remainder - $packets[1];
+
+            }
+
+        // Calculate what packs to send
+        }else{
+            $highestDeduction = 0;
+            foreach($flipPackets as $packet) {
+                
+                // Calculates the amount of widgets that are remaining to be packed
+                $valueLeft = $remainder - $packet;
+
+                if($valueLeft > 0){
+
+                    if($packet >= $highestDeduction){
+                        $highestDeduction = $packet;
+                        $pickedValue = $packet;
+                    }
+
+                }else if($valueLeft === 0){
+
+                    $highestDeduction = $packet;
+                    $pickedValue = $packet;
+
+                }else{
+
+                    // Take the smallest value i the array and sets picked value to this so that it doesn't carry the last looped value
+                    $pickedValue = $packets[0];
+
+                }
+            }
+        }
+
+        // Makes sure that if no value is picked that a 0 is not added onto the picked packets array
+        if($pickedValue > 0){
+             array_push($pickedPackets, $pickedValue);
+        }
+        
+        // Calculates the remaining value left ready for the next loop
+        $remainder = $remainder - $pickedValue;
+
+        // Sets picked value back to 0
+        $pickedValue = 0;
     }
 
-    return $widgetsRequired;
+    return $pickedPackets;
 }
-
 ?>
